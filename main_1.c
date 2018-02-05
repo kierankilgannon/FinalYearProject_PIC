240c7e05821faa42d138cef2905b7f1c7b292d65
kierankilgannon <32903766+kierankilgannon@users.noreply.github.com>
2018-02-05 19:58:11 +0000

    Add files via upload

--- main_1.c
+++ main_1.c
@@ -1,15 +1,11 @@
 /**
   Generated Main Source File
-
   Company:
     Microchip Technology Inc.
-
   File Name:
     main.c
-
   Summary:
     This is the main file generated using PIC10 / PIC12 / PIC16 / PIC18 MCUs 
-
   Description:
     This header file provides implementations for driver APIs for all modules selected in the GUI.
     Generation Information :
@@ -24,13 +20,11 @@
 /*
     (c) 2016 Microchip Technology Inc. and its subsidiaries. You may use this
     software and any derivatives exclusively with Microchip products.
-
     THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
     EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
     WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
     PARTICULAR PURPOSE, OR ITS INTERACTION WITH MICROCHIP PRODUCTS, COMBINATION
     WITH ANY OTHER PRODUCTS, OR USE IN ANY APPLICATION.
-
     IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
     INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
     WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
@@ -38,7 +32,6 @@
     FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
     ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
     THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
-
     MICROCHIP PROVIDES THIS SOFTWARE CONDITIONALLY UPON YOUR ACCEPTANCE OF THESE
     TERMS.
 */
@@ -56,18 +49,15 @@
 
 
 unsigned volatile int  tick_count;
-unsigned  int yourtime1=0;
-unsigned  int yourtime2=0;
+unsigned  volatile int yourtime1=0;
+unsigned  volatile int yourtime2=0;
 int buttonpress=0;
 int flag1=0;
 int flag2=0;
 int interval=0;
 
-//void interrupt TMR0_ISR(){
-//    TMR0IF=0;
-//    tick_count++;
-//}
-
+void interrupt INTERRUPT_InterruptManager(void);
+void interrupt TMR0_MyISR(void);
         
     // add your TMR0 interrupt custom code
     // or set custom function using TMR0_SetInterruptHandler()
@@ -117,8 +107,15 @@ void main(void)
 }
 
 
-//#include "interrupt_manager.h"
-//#include "mcc.h"
+
+//void interrupt TMR0_MyISR (void)
+//{
+//    if(TMR0IF)
+//    {
+//        TMR0IF=0;
+//        tick_count++;
+//    }
+//}
 
 void interrupt INTERRUPT_InterruptManager (void)
 {
@@ -131,7 +128,6 @@ void interrupt INTERRUPT_InterruptManager (void)
     {
         yourtime1 = tick_count;
         flag1=1;
-        buttonpress++;
         PIN_MANAGER_IOC();
         INTCONbits.IOCIF =0;
     }
@@ -143,8 +139,3 @@ void interrupt INTERRUPT_InterruptManager (void)
         INTCONbits.INTF=0;
     }
 }
-//}
-/**
- End of File
-*/
-
