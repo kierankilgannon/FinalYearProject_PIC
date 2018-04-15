e467e4e0bd3d82162e759e821398c363be38910f
kierankilgannon <32903766+kierankilgannon@users.noreply.github.com>
2018-04-15 16:49:33 +0100

    Add files via upload

--- main_1.c
+++ main_1.c
@@ -1,39 +1,7 @@
-/**
-  Generated Main Source File
-  Company:
-    Microchip Technology Inc.
-  File Name:
-    main.c
-  Summary:
-    This is the main file generated using PIC10 / PIC12 / PIC16 / PIC18 MCUs 
-  Description:
-    This header file provides implementations for driver APIs for all modules selected in the GUI.
-    Generation Information :
-        Product Revision  :  PIC10 / PIC12 / PIC16 / PIC18 MCUs  - 1.45
-        Device            :  PIC16F1823
-        Driver Version    :  2.00
-    The generated drivers are tested against the following:
-        Compiler          :  XC8 1.35
-        MPLAB             :  MPLAB X 3.40
-*/
-
 /*
-    (c) 2016 Microchip Technology Inc. and its subsidiaries. You may use this
-    software and any derivatives exclusively with Microchip products.
-    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
-    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
-    WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
-    PARTICULAR PURPOSE, OR ITS INTERACTION WITH MICROCHIP PRODUCTS, COMBINATION
-    WITH ANY OTHER PRODUCTS, OR USE IN ANY APPLICATION.
-    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
-    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
-    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
-    BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
-    FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
-    ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
-    THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
-    MICROCHIP PROVIDES THIS SOFTWARE CONDITIONALLY UPON YOUR ACCEPTANCE OF THESE
-    TERMS.
+    * File Name: main_1.c
+    * Author: Kieran Kilgannon
+    * Trip time in milliseconds recorded and sent to the Raspberry PI 
 */
 
 #include <pic16f1824.h>
@@ -55,10 +23,6 @@ unsigned int interval = 0;
 int x=0;
 
 void interrupt InterruptServiceRoutine(void);
-//void interrupt TMR0_MyISR(void);
-        
-    // add your TMR0 interrupt custom code
-    // or set custom function using TMR0_SetInterruptHandler()
 
 void main(void)
 {
@@ -67,29 +31,13 @@ void main(void)
     InitUSART();
     InitialiseTimers();
 
-    // Enable the Global Interrupts
-    //INTERRUPT_GlobalInterruptEnable();
-    INTCONbits.GIE = 1;
-
-    // Enable the Peripheral Interrupts
-    //INTERRUPT_PeripheralInterruptEnable();
-    INTCONbits.PEIE = 1;
-
-    // Disable the Global Interrupts
-    //INTERRUPT_GlobalInterruptDisable();
-
-    // Disable the Peripheral Interrupts
-    //INTERRUPT_PeripheralInterruptDisable();
+    INTCONbits.GIE = 1;  //Global Interrupt Enable
+    INTCONbits.PEIE = 1;  //Peripheral Interrupt Enable
 
     printf("READY\r\n");
-    //INTCONbits.INTE =0;
+   
     while(1)
     {
-//        if(buttonpress)
-//        {
-//            printf("%i", buttonpress);
-//            buttonpress = 0;
-//        }
         if(flag1==1)
         { 
             while(flag2==0){} //wait for second interrupt !flag2
@@ -97,19 +45,14 @@ void main(void)
             //printf("time  2: %i ",yourtime2);
             __delay_ms(100);
             interval = yourtime2 - yourtime1;
-            printf("\r\n TRIPTIME: %i",(yourtime2-yourtime1));
-            printf("Reg is: %i .... ", x);
+            printf("Triptime: %i Reg is: %i \r\n",(yourtime2-yourtime1),(x));
+            //printf(" Reg is: %i \r\n", x);
             __delay_ms(1000);
             flag1 = 0;
             flag2 = 0;
-            //tick_count =0;
-            //__delay_ms(5000);
-//            INTCONbits.IOCIE = 1;
-//            INTCONbits.INTE = 1;
-//             
         }   
     }  
-}
+}//End main
 
 
 void interrupt InterruptServiceRoutine(void)
@@ -120,26 +63,6 @@ void interrupt InterruptServiceRoutine(void)
         //TMR0=117;
         TMR0IF=0;
     }
-//    if(INTCONbits.IOCIE == 1 && INTCONbits.IOCIF == 1)
-//    {
-//        yourtime1 = tick_count;
-//        printf("interrupt");
-//        //tick_count = 0;
-//        //flag1=1;
-//        PIN_MANAGER_IOC();
-//        //IOCAFbits.IOCAF4 = 0;
-//        INTCONbits.IOCIF = 0;
-//        //flag1=0;   
-//        //INTCONbits.IOCIE = 0; //disable interrupt
-//        //printf("pressed");
-//        buttonpress++;
-//    }
-//    if(IOCAFbits.IOCAF4 == 1 )
-//    {
-//        buttonpress++;
-//        IOCAFbits.IOCAF4 = 0;
-//    }  
-    
     
     if(IOCAFbits.IOCAF4 == 1 )
     {
@@ -158,11 +81,7 @@ void interrupt InterruptServiceRoutine(void)
     {
         yourtime2 = tick_count;
         x = TMR0;
-        //INT_ISR();
         INTCONbits.INTF = 0;
-        //INTCONbits.INTE = 0;
-        //buttonpress++;
         flag2 = 1;
-        //printf("2");
     }
-}
+}//End ISR
